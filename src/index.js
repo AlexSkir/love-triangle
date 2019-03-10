cd3f52a2b62d3ecf2abb13d7edf076aa30078240
AlexSkir <a.skirnevskaia@gmail.com>
2019-03-11 00:31:48 +0300

    feat: implement function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var summ = 0;   
+  for (i = 0; i<preferences.length; i++) { 
+    let k = preferences[i] - 1;
+    if (k == i) continue;
+    let n = preferences[k] - 1;
+    if (k == n) continue;
+    let m = preferences[n] - 1;
+    if (n == m) continue;
+    if (m==i) {
+        summ++;
+      }        
+  }
+  return summ/3;
 };
